c70a02d9dd45b35a9bed3e906b60d9f68cf26770
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-27 22:57:20 +0100

    make structure for themes and posts

--- components-training/src/app/posts-list/posts-list.component.ts
+++ components-training/src/app/posts-list/posts-list.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface User {
+  _id: string;
+  username: string;
+}
+
+interface Theme {
+  _id: string;
+  name: string;
+}
+
+interface Post {
+  likes: string[];
+  _id: string;
+  text: string;
+  userId: User;
+  themeId: Theme;
+  created_at: string;
+  updatedAt: string;
+  __v: number;
+}
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.css'
 })
 export class PostsListComponent {
-
+  posts: Post[] = [];
 }

e8256e1166cb514d8c9c1aa203d1b45dbc4b6ddc
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-28 01:04:46 +0100

    inject in service file

--- components-training/src/app/api.service.ts
+++ components-training/src/app/api.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class ApiService {
 
   constructor() { }
+  private http = inject(HttpClient)
 }

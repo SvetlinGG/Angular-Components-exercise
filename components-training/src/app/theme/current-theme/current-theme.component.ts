853dee69a6932791ad6cd2e207a66ebbf4df1f6b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-31 22:49:27 +0200

    create current theme component

---
+++ components-training/src/app/theme/current-theme/current-theme.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-theme',
+  imports: [],
+  templateUrl: './current-theme.component.html',
+  styleUrl: './current-theme.component.css'
+})
+export class CurrentThemeComponent {
+
+}

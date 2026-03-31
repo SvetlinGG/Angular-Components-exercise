697a8606bf551eaeece6b09eb864004fc0f010ed
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-31 23:49:59 +0200

    fix an issues

--- components-training/src/app/app.routes.ts
+++ components-training/src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './user/register/register.component';
 import { LoginComponent } from './user/login/login.component';
 import { ProfileComponent } from './user/profile/profile.component';
-import { HeaderComponent } from './core/header/header.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
 
-    {path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path: 'home', component: HeaderComponent},
+    {path: '', component: MainComponent },
 
     // User
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
     {path: 'profile', component: ProfileComponent},
+
 ];
--- components-training/src/app/main/main.component.ts
+++ components-training/src/app/main/main.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { ThemesListComponent } from '../theme/themes-list/themes-list.component';
 import { PostsListComponent } from '../posts-list/posts-list.component';
-import { RouterLink } from '@angular/router';
-import { HeaderComponent } from '../core/header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ThemesListComponent, PostsListComponent,HeaderComponent ],
+  imports: [ThemesListComponent, PostsListComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })

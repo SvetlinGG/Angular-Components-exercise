f89f9920bea9adcf827ad8609cbae4e23b677706
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-01 01:01:38 +0200

    setup path

--- components-training/src/app/main/main.component.ts
+++ components-training/src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ThemesListComponent } from '../theme/themes-list/themes-list.component';
 import { PostsListComponent } from '../posts-list/posts-list.component';
+import { HeaderComponent } from '../core/header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ThemesListComponent, PostsListComponent],
+  imports: [ThemesListComponent, PostsListComponent, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })

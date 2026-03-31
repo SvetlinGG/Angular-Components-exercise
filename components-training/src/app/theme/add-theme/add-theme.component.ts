0a60c3cc9c3c6659f13e4b833ba5aca24168384b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-31 22:50:57 +0200

    create add-theme component

---
+++ components-training/src/app/theme/add-theme/add-theme.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-theme',
+  imports: [],
+  templateUrl: './add-theme.component.html',
+  styleUrl: './add-theme.component.css'
+})
+export class AddThemeComponent {
+
+}

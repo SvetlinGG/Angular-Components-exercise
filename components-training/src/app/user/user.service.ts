e88bcb60ba3fe670c9f2b9edbf00bc7e9d90d0c5
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-01 12:00:31 +0200

    setup in user service - login

--- components-training/src/app/user/user.service.ts
+++ components-training/src/app/user/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { UserForAuth } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  USER_KEY = '[user]';
+  user: UserForAuth | null = null;
+  
+  constructor() {
+    try {
+      const lsUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(lsUser)
+    } catch (error) {
+      this.user = null;
+    }
+  }
+  login(){
+    this.user = {
+      firstName: 'Svetlin',
+      email: 'svetlin@abv.bg',
+      phoneNumber: '123123123',
+      password: 'asdasd',
+      id: '123123'
+    }
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user))
+  }
+
+  
+
+
 }

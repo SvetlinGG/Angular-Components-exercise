27121357f30170fb9c1a81edfd0a3501e35cd531
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-01 22:56:02 +0200

    routing to add-theme

--- components-training/src/app/app.routes.ts
+++ components-training/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './user/login/login.component';
 import { ProfileComponent } from './user/profile/profile.component';
 import { MainComponent } from './main/main.component';
 import { ErrorComponent } from './error/error.component';
+import { CurrentThemeComponent } from './theme/current-theme/current-theme.component';
+import { AddThemeComponent } from './theme/add-theme/add-theme.component';
 
 export const routes: Routes = [
 
@@ -13,6 +15,13 @@ export const routes: Routes = [
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
     {path: 'profile', component: ProfileComponent},
+
+    //Theme routing
+    {path: 'themes', children: [
+        {path: '', component: MainComponent},
+        {path: ':themeId', component: CurrentThemeComponent},
+    ]},
+    {path: 'add-theme', component: AddThemeComponent },
 
     {path: '**', component: ErrorComponent}
 
--- components-training/src/app/theme/add-theme/add-theme.component.ts
+++ components-training/src/app/theme/add-theme/add-theme.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../api.service';
 import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-theme',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './add-theme.component.html',
   styleUrl: './add-theme.component.css'
 })
 export class AddThemeComponent {
+
+  constructor(private apiService: ApiService){}
+
+  addTheme(event: Event, themeName: string, postText: string){
+    event.preventDefault();
+
+    this.apiService.createTheme(themeName, postText).subscribe(data => {
+      console.log(data);
+      
+    })
+  }
 
 }

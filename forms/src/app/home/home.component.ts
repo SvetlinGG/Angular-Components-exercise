357b2319e5e3c1e42d05ad998fb5bf3ca6a3d3c3
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-03 07:45:03 +0200

    setup path

--- forms/src/app/home/home.component.ts
+++ forms/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { HighlightDirective } from "../directives/hughlight.directives";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, HighlightDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

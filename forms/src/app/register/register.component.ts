72941af5dba4813559a4130c9cd35bcac3d692bf
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-05 00:55:09 +0200

    validation and conditional statement

--- forms/src/app/register/register.component.ts
+++ forms/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,14 +10,23 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angul
 export class RegisterComponent {
 
   registerForm = new FormGroup ({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.maxLength(5)]),
   })
 
   constructor(private fb: FormBuilder){}
 
   handleSubmit(){
+
+    if (this.registerForm.invalid){
+      console.log('Form is invalid!');
+      return;
+    }
+    console.log(this.registerForm.value);
     
+
+    this.registerForm.reset();
+
   }
 
 }
